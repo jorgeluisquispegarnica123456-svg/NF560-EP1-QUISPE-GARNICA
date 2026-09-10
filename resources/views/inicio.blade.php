83ea36bf2b708c49c42d6b13e7d61ee7add727b1
Garnica23 <jorgeluisquispegarnica123456@gmail.com>
2026-09-10 10:17:09 -0400

    corrigiendo error
    Removed comment about featured books listing.

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -37,7 +37,6 @@
             </div>
         </header>
 
-        <!-- Listado de Destacados únicamente (Tarea 05) -->
         <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
             <h2 class="text-2xl font-bold mb-6 text-gray-900">Libros Destacados</h2>
 
